5ca1795e18ea1d3ca59e9bd222cbca7f1a74a994
igor-zarudniy <123390484+igor-zarudniy@users.noreply.github.com>
2025-10-31 10:34:12 +0100

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -133,7 +133,10 @@ exports.handler = async (event, context) => {
         } else if (jsonData.error.includes('сервера')) {
           statusCode = 500;
         }
-      } else if (jsonData.status === 'created' || jsonData.status === 'updated') {
+      } else if (jsonData.status === 'created'){
+        statusCode = 200;
+      }
+      elese  if(jsonData.status === 'updated') {
         statusCode = 250;
       }
     } catch (e) {
